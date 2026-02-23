6428941971f7f8a0f81d8aa978beb3c4e2980b9d
NCRMN170 <kmet4686@gmail.com>
2026-02-23 16:09:16 +0300

    Add files via upload

---
+++ Android.mk
@@ -0,0 +1,10 @@
+
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),TB132FU)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
+
+
+
---
+++ vendorsetup.sh
@@ -0,0 +1,85 @@
+#
+#   OrangeFox Recovery Project - vendorsetup.sh for Lenovo TB132FU
+#
+FDEVICE="TB132FU"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w \"$FDEVICE\")
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w \"$FDEVICE\")
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   echo "** WARNING **: Always set FOX_BUILD_DEVICE to the device codename before starting to build for any device!"
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
+        # --- Temel Derleme Ayarları ---
+        export TW_DEFAULT_LANGUAGE="en"
+        export LC_ALL="C"
+        export ALLOW_MISSING_DEPENDENCIES=true
+        export FOX_BUILD_TYPE="Unofficial"
+        export OF_MAINTAINER="NCRMN170" 
+
+        # --- Sürüm Bilgileri ---
+        export FOX_MANIFEST_VER="12.1"
+        export FOX_ARCH="arm64"
+        export FOX_TARGET_DEVICES="TB132FU,p11_pro_2gen"
+        
+        
+        export FOX_VIRTUAL_AB_DEVICE="1"
+
+        export OF_USE_LZ4_COMPRESSION="1"
+ 
+        export OF_ENABLE_ALL_PARTITION_TOOLS="1"
+        export OF_USE_DMCTL="1"
+
+        export OF_USE_MAGISKBOOT="1"
+        export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+        export OF_PATCH_AVB20="1"
+
+        export OF_SCREEN_H=2560 
+        
+        export OF_ALLOW_DISABLE_NAVBAR=1 
+        export OF_USE_DYNAMIC_FULLSCREEN=1
+
+        export FOX_ENABLE_LPTOOLS="1"          
+        export OF_ALLOW_DISABLE_NAVBAR="1"     
+        #export OF_FLASHLIGHT_ENABLE="1"       
+        #export OF_FL_PATH1="/tmp/flashlight"
+        
+        # --- Özellikler (Sadeleştirme) ---
+        export FOX_DELETE_AROMAFM="1"
+        
+        # --- Binary ve Araçlar ---
+        export FOX_USE_SED_BINARY="1"
+        export FOX_USE_XZ_UTILS="1"
+        export FOX_USE_GREP_BINARY="1"
+        export FOX_USE_BASH_SHELL="1"
+        export FOX_ASH_IS_BASH="1"
+        export FOX_USE_TAR_BINARY="1"
+        export FOX_USE_NANO_EDITOR="1"
+        
+        # --- OTA ve Sistem Korumaları ---
+        export OF_KEEP_DM_VERITY="1"
+        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR="1"
+        export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+        export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+        export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1" 
+        
+        # --- Loglama ---
+        if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+           export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+           export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+           export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+           export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+        fi
+fi
+
+
